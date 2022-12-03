9b0fae1a244e077d49f682a3abaf5408e7220c20
Marina Popovich <fevera8076@gmail.com>
2022-12-03 18:49:40 +0300

    Создано

---
+++ Lesson6/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Lesson6
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+    public class Person
+    {
+       
+
+        private string Name { get; set; }
+        public string name { get; set; }
+        private string Surname { get; set; }
+        private int Age { get; set; }
+        public int Salary { get; set; }
+         public Person(string name)
+        {
+            this.name = name;
+        }
+
+    }
+    public class Scientist : Person
+    {
+        public int Academ { get; set; }
+
+        public override string name
+        {
+            get { return name; }
+           
+        }
+      public void WriteCharacteristics()
+        {
+            Console.WriteLine(" $ Имя: {name} , Фамилия: {Surname}, Возраст: {Age} , Ученая степень: {Academ}, Зарплата: {Salary}");
+        }
+    }   
+
+       
+    
+}
